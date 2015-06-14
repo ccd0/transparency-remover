6d905c692116da1f5e22cd2ce92d5828a4941bff
ccd0 <admin@containerchan.org>
2015-06-13 21:35:33 -0700

    Release v0.1.0.

--- transparency-remover.meta.js
+++ transparency-remover.meta.js
@@ -1,9 +1,9 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name        Transparency Remover
 // @namespace   https://github.com/ccd0
 // @description Reveals the colors in the transparent regions of PNG images. Activated by a context menu item.
 // @match       *://*/*
-// @version     0.0.4
+// @version     0.1.0
 // @grant       GM_xmlhttpRequest
 // @updateURL 	https://ccd0.github.io/transparency-remover/transparency-remover.meta.js
 // @downloadURL https://ccd0.github.io/transparency-remover/transparency-remover.user.js
